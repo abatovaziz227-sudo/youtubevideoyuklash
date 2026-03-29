effcf14104002521551b8d3ca614f5d752895ba3
abatovaziz227-sudo <abatovaziz227@gmail.com>
2026-03-29 20:34:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,33 @@
+import instaloader
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
+TOKEN = "8424991362:AAGTzrYZBVXM9RWDE6LN5HsnFerJecSzyRw"
+
+L = instaloader.Instaloader()
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text.strip()
+
+    if "instagram.com" in text:
+        username = text.rstrip("/").split("/")[-1]
+    else:
+        username = text
+
+    try:
+        profile = instaloader.Profile.from_username(L.context, username)
+
+        L.download_profile(username, profile_pic_only=True)
+
+        file_path = f"{username}/{username}_profile_pic.jpg"
+
+        await update.message.reply_photo(photo=open(file_path, "rb"))
+
+    except Exception as e:
+        await update.message.reply_text("❌ Profil topilmadi yoki yopiq!")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+app.run_polling()
